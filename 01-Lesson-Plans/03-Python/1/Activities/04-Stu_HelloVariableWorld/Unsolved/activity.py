2cf786d9c2d2757d7cdab984ace5815f088c9d60
dkasatchkow <d.kasatchkow@gmail.com>
2021-06-22 22:30:55 +0800

    03-Python-01 Added solved activity files

---
+++ 01-Lesson-Plans/03-Python/1/Activities/04-Stu_HelloVariableWorld/Unsolved/activity.py
@@ -0,0 +1,13 @@
+name = "Linda"
+country = "Afghanistan"
+age = 57
+hourly_wage = 25
+satisfied = True
+daily_wage = hourly_wage * 8
+
+print("The name is "+name)
+print("country is "+country)
+print("age is "+str(age))
+
+print(f"The name is {name}")
+print(f"daily wage is {daily_wage}")
